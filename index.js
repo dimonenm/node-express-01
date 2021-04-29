a19182ab2186ffa6287620528ad5db3a52d053e3
exp <arzuakov@yahoo.com>
2021-04-30 02:45:54 +0300

    h30.04.2021

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import path from 'path';
 
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 4000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send(`<h1>Server has been started on ${PORT} port</h1>`);
+})
+
+app.get('/api/db/', (req, res) => {
+    res.append('Content-Type', 'application/json')
+    res.append('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.append('Access-Control-Allow-Methods', 'GET')
+    res.sendFile(path.resolve(__dirname, 'static', 'db', 'db.json'));
 })
 
 app.listen(PORT, () => {
