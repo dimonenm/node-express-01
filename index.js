9c3416c126edd4c36da2db45735f10776cd2d890
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-05-28 17:49:09 +0300

    w28.05.2021

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ import fs from "fs";
 import express from "express";
 import path from 'path';
 
-
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 4000;
 const app = express();
 
 app.use(express.json({ limit: '25mb' })) // for parsing application/json
 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'exp', 'index.html'));
+});
+app.get('/static/css/main.e90a555f.chunk.css', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'exp', 'static', 'css', 'main.e90a555f.chunk.css'));
+});
+app.get('/static/js/2.cd979bb9.chunk.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'exp', 'static', 'js', '2.cd979bb9.chunk.js'));
+});
+app.get('/static/js/main.34801c8a.chunk.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'exp', 'static', 'js', 'main.34801c8a.chunk.js'));
+});
+
+
 app.get('/api/db/', (req, res) => {
     res.append('Content-Type', 'application/json')
     res.append('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // res.append('Access-Control-Allow-Origin', 'http://localhost')
     res.append('Access-Control-Allow-Methods', 'GET')
     res.sendFile(path.resolve(__dirname, 'static', 'db', 'db.json'));
 });
